544fca1faff5135a9de5659248617403e91b54dd
77happy <2258362309@qq.com>
2020-12-05 14:57:10 +0800

    manageAdd sucessfully

--- src/views/index/Managedo/putMed.vue
+++ src/views/index/Managedo/putMed.vue
@@ -24,7 +24,7 @@
               v-for="(item1, index1) in item.child"
               :key="index1"
             >
-              {{ item1.id }}
+              <!-- {{ item1.id }} -->
             </div>
           </div>
         </div>
@@ -57,43 +57,100 @@
         </div>
       </div>
       <div class="stept2">
-        <div class="chosetitle">2.放置药物</div>
-        <div class="content">
-          <el-table :data="tableData" border style="width: 100%">
-            <el-table-column fixed prop="mid" label="药品编号" width="100">
-            </el-table-column>
-            <el-table-column prop="name" label="药品名称" width="100">
-            </el-table-column>
-            <el-table-column prop="province" label="规格" width="100">
-            </el-table-column>
-            <el-table-column prop="city" label="有效期" width="100">
-            </el-table-column>
-            <el-table-column prop="address" label="批号" width="100">
-            </el-table-column>
-            <el-table-column prop="zip" label="用法" width="100">
-            </el-table-column>
-            <el-table-column prop="zip" label="价格" width="100">
-            </el-table-column>
-            <el-table-column prop="zip" label="分类" width="100">
-            </el-table-column>
-            <el-table-column fixed="right" label="操作" width="100"> 
-              <template slot-scope="scope">
-                <el-button
-                  @click="handleClick(scope.row)"
-                  type="text"
-                  size="small"
-                  >修改</el-button
-                >
-                <el-button type="text" size="small">删除</el-button>
-              </template>
-            </el-table-column>
-          </el-table>
-          <div class="flex justify-center">
-            <el-button class="add">add</el-button>
+        <div class="chosetitle">2.查看药物</div>
+        <div>
+          <div class="content">
+            <el-table :data="tableData" border style="width: 100%">
+              <el-table-column prop="mid" label="药品编号" width="100">
+              </el-table-column>
+              <el-table-column prop="name" label="药品名称" width="100">
+              </el-table-column>
+              <el-table-column prop="province" label="规格" width="100">
+              </el-table-column>
+              <el-table-column prop="city" label="有效期" width="100">
+              </el-table-column>
+              <el-table-column prop="address" label="批号" width="100">
+              </el-table-column>
+              <el-table-column prop="zip" label="用法" width="100">
+              </el-table-column>
+              <el-table-column prop="zip" label="价格" width="100">
+              </el-table-column>
+              <el-table-column prop="zip" label="分类" width="100">
+              </el-table-column>
+              <el-table-column label="操作" width="100">
+                <template slot-scope="scope">
+                  <el-button
+                    @click="handleClick(scope.row)"
+                    type="text"
+                    size="small"
+                    >修改</el-button
+                  >
+                  <el-button type="text" size="small">删除</el-button>
+                </template>
+              </el-table-column>
+            </el-table>
           </div>
+        </div>
+
+        <div class="flex justify-center">
+          <el-button class="add" @click="dialogFormVisible = true"
+            >add</el-button
+          >
         </div>
       </div>
     </div>
+    <el-dialog title="新增药物" :visible.sync="dialogFormVisible" width="30%">
+      <div class="flex justify-center">
+        <el-form :model="form" label-width="80px">
+          <el-form-item label="药品编号" :label-width="formLabelWidth">
+            <el-input v-model="form.mid" autocomplete="off"></el-input>
+          </el-form-item>
+
+          <el-form-item label="药品名称" :label-width="formLabelWidth">
+            <el-input v-model="form.name" autocomplete="off"></el-input>
+          </el-form-item>
+
+          <el-form-item label="药品规格" :label-width="formLabelWidth">
+            <el-input
+              v-model="form.specifications"
+              autocomplete="off"
+            ></el-input>
+          </el-form-item>
+
+          <el-form-item label="有效期" :label-width="formLabelWidth">
+            <el-input v-model="form.term" autocomplete="off"></el-input>
+          </el-form-item>
+
+          <el-form-item label="批号" :label-width="formLabelWidth">
+            <el-input v-model="form.batch" autocomplete="off"></el-input>
+          </el-form-item>
+
+          <el-form-item label="服用说明" :label-width="formLabelWidth">
+            <el-input v-model="form.dose" autocomplete="off"></el-input>
+          </el-form-item>
+
+          <el-form-item label="价格" :label-width="formLabelWidth">
+            <el-input v-model="form.price" autocomplete="off"></el-input>
+          </el-form-item>
+
+          <el-form-item label="分类" :label-width="formLabelWidth">
+            <el-input
+              v-model="form.classification"
+              autocomplete="off"
+            ></el-input>
+          </el-form-item>
+
+          <el-form-item label="抽屉" :label-width="formLabelWidth">
+            <el-input v-model="form.cabinet" autocomplete="off"></el-input>
+          </el-form-item>
+        </el-form>
+      </div>
+
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="dialogFormVisible = false">取 消</el-button>
+        <el-button type="primary" @click="addMed">确 定</el-button>
+      </div>
+    </el-dialog>
   </div>
 </template>
 
@@ -110,61 +167,72 @@ export default {
       nowDrawId: -1, //当前选中抽屉的id
       boxCheckList: [], //当前选中的盒子列
 
-      machineItemList: [
-        
-      ], //左侧 抽屉整体布局
+      machineItemList: [], //左侧 抽屉整体布局
 
       tableData: [
         {
           date: "2016-05-02",
           name: "王小虎",
-          address: "上海市普陀区金沙江路 1518 弄",
+          address: "上海市",
         },
         {
           date: "2016-05-04",
           name: "王小虎",
-          address: "上海市普陀区金沙江路 1517 弄",
+          address: "上海市",
         },
         {
           date: "2016-05-01",
           name: "王小虎",
-          address: "上海市普陀区金沙江路 1519 弄",
+          address: "上海市",
         },
         {
           date: "2016-05-03",
           name: "王小虎",
-          address: "上海市普陀区金沙江路 1516 弄",
+          address: "上海市",
         },
       ],
-      search: "",
+
+      dialogFormVisible: false,
+      form: {
+        mid: "",
+        name: "",
+        specifications: "",
+        term: "",
+        batch: "",
+        dose: "",
+        price: "",
+        classification: "",
+        cabinet: "",
+      },
+      formLabelWidth: "70px",
     };
   },
   // created是组件创建的时候调用该方法 组件被创建的时候去获取药品列表 getMachineItemList 是自己写的一个方法
   created() {
-    this.getMachineItemList()
+    this.getMachineItemList();
   },
   methods: {
     //获取药箱布局 假设为9行*3列
-    getMachineItemList(){
-      let arr=[];
-      for(let i=0;i<9;i++){
+    getMachineItemList() {
+      let arr = [];
+      for (let i = 0; i < 9; i++) {
         //设置每一行的属性
-        let rowObj={
-          id:i+1,
-          child:[]
+        let rowObj = {
+          id: i + 1,
+          child: [],
         };
-        for(let j=0;j<3;j++){
+        for (let j = 0; j < 3; j++) {
           //设置每一列的属性
-          let colObj={
-            id:(i+1)+'-'+(j+1)
-          }
+          let colObj = {
+            id: i + 1 + "-" + (j + 1),
+          };
           //push为数组方法，增加一个项
-          rowObj.child.push(colObj)
+          rowObj.child.push(colObj);
         }
-        arr.push(rowObj)
+        arr.push(rowObj);
       }
-      console.log(arr)
-      this.machineItemList=arr;
+      console.log(arr);
+      this.machineItemList = arr;
     },
 
     //当某一列被点击后，选中该行
@@ -177,6 +245,43 @@ export default {
     },
     handleDelete(index, row) {
       console.log(index, row);
+    },
+
+    addMed() {
+      let mid = this.form.mid;
+      let name = this.form.name;
+      let dose = this.form.dose;
+      let price = this.form.price;
+      let classification = this.form.classification;
+      let cabinet = this.form.cabinet;
+      let specifications = this.form.specifications;
+      let batch = this.form.batch;
+      let term = this.form.term;
+
+      // console.log(mid)
+      // console.log(name)
+      // 传给后台接口
+      this.$http
+        .get("/putmed/putMedicine", {
+          mid: mid,
+          name: name,
+          dose: dose,
+          price: price,
+          classification: classification,
+          cabinet: cabinet,
+          specifications: specifications,
+          batch: batch,
+          term: term,
+        })
+        .then((res) => {
+          if (res.code == 1) {
+            alert("新增成功！");
+          } else {
+            alert("新增失败！！");
+          }
+
+          this.dialogFormVisible = false;
+        });
     },
   },
 };
@@ -208,12 +313,35 @@ export default {
           .row {
             //行布局
             display: flex;
-            align-items: center;
             justify-content: space-between;
+            align-items: center;
+            margin-bottom: 10px;
+            position: relative;
+            > .row_arrow {
+              display: none;
+              position: absolute;
+              left: -30px;
+              top: 10px;
+            }
+            &:last-child {
+              margin-bottom: 0;
+            }
+            &:hover,
+            &.active {
+              > .row_arrow {
+                display: block;
+              }
+            }
             .col {
-              //行内每一项的布局
-              width: 100px;
-              height: 50px;
+              width: 88px;
+              height: 44px;
+              background: rgba(231, 211, 49, 1);
+              &.have {
+                background: rgba(32, 159, 133, 1);
+              }
+              &.none {
+                background: rgba(90, 134, 239, 1);
+              }
             }
           }
         }
@@ -257,8 +385,6 @@ export default {
       }
       // &.step2 {
       //   >.content{
-
-      //   // margin-top: 10px;
       //   }
       // }
 
@@ -268,7 +394,6 @@ export default {
         color: rgba(255, 255, 255, 1);
         margin-top: 70px;
       }
-     
     }
   }
 }
